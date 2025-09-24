018501e25ac860d953bde238846891dba2e53688
Naveen Upreti <90297133+naveenupreti@users.noreply.github.com>
2025-09-24 08:35:59 +0530

    Update multiplication_table.py

--- Programs/UNIT-1/multiplication_table.py
+++ Programs/UNIT-1/multiplication_table.py
@@ -10,8 +10,8 @@ for num in range(start, end + 1):
     print("\nMultiplication Table of {}".format(num))
     print("-" * 25)
     
-    # Multiplication from 1 to 10
-    for i in range(1, 11):
-        # Align numbers nicely using format specifiers
-        #print(f"{num:>3} x {i:>2} = {num*i:>4}") # python 3.6 and above
-        print("{:>3} x {:>2} = {:>4}".format(num, i, num*i))
+# Multiplication from 1 to 10
+for i in range(1, 11):
+    # Align numbers nicely using format specifiers
+    #print(f"{num:>3} x {i:>2} = {num*i:>4}") # python 3.6 and above
+    print("{:>3} x {:>2} = {:>4}".format(num, i, num*i))
